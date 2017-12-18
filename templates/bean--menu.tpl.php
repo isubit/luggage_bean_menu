a79cb146c10761f5c4154058248cc91303a2f796
Carney, Sarah A <sacarney@iastate.edu>
2017-12-18 13:30:32 -0600

    LUGG-1046 Add template, css, and new fields

--- templates/bean--menu.tpl.php
+++ templates/bean--menu.tpl.php
@@ -1,1 +1,14 @@
+<nav role="navigation" class="bean-menu <?php print $bean->field_menu_orientation['und'][0]['value'] ?>">
+  <?php if ($bean->title !== ''): ?>
+    <p class="bean-menu_title"><?php print $title; ?></p>
+  <?php endif ?>
 
+  <?php if ($bean->field_menu_link) :?>
+    <ul>
+      <?php $items = field_get_items('bean', $bean, 'field_menu_link');
+      foreach ($items as $item) {
+        echo'<li><a href="' . $item['url'] . '">ITEM</a></li>';
+      } ?>
+    </ul>
+  <?php endif ?>
+</nav>
