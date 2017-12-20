1919643515761faa487c1735449e09e3d2664580
Carney, Sarah A <sacarney@iastate.edu>
2017-12-20 12:23:28 -0600

    LUGG-1046 Removed title (redundant), and replaced placeholder ITEM with the actual link name.

--- templates/bean--menu.tpl.php
+++ templates/bean--menu.tpl.php
@@ -1,13 +1,9 @@
 <nav role="navigation" class="bean-menu <?php print $bean->field_menu_orientation['und'][0]['value'] ?>">
-  <?php if ($bean->title !== ''): ?>
-    <p class="bean-menu_title"><?php print $title; ?></p>
-  <?php endif ?>
-
   <?php if ($bean->field_menu_link) :?>
     <ul>
       <?php $items = field_get_items('bean', $bean, 'field_menu_link');
       foreach ($items as $item) {
-        echo'<li><a href="' . $item['url'] . '">ITEM</a></li>';
+        echo'<li><a href="' . $item['url'] . '">' . $item['title'] . '</a></li>';
       } ?>
     </ul>
   <?php endif ?>
